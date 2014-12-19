863cec44283dbc1ff0e1a7752641fcf73c346ee3
Paul Bernard <paul.bernard@teleapp.com>
2014-12-18 20:18:52 -0500

    init

---
+++ SpectrumXl/ApiException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Org.Quantintel.Spectrum
+{
+    public class ApiException : Exception
+    {
+
+        private int errorCode = 0;
+
+        public ApiException() { }
+
+        public int ErrorCode
+        {
+            get
+            {
+                return errorCode;
+            }
+        }
+
+        public ApiException(int errorCode, string message)
+            : base(message)
+        {
+            this.errorCode = errorCode;
+        }
+    }
+}
+
