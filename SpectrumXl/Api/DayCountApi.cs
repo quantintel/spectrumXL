a8bd6eae56097f7c2171c2ad19efaaa1ecdf35ce
Paul Bernard <paul.bernard@teleapp.com>
2014-12-29 15:44:56 -0500

    type

--- SpectrumXl/Api/DayCountApi.cs
+++ SpectrumXl/Api/DayCountApi.cs
@@ -32,59 +32,6 @@ namespace Org.Quantintel.Spectrum.Api
         }
 
         /// <summary>
-        /// day count fraction using the Actual/360F method returns the day count fraction given a start and end date as serial numbers
-        /// </summary>
-        /// <param name="fromdate">from date</param>
-        /// <param name="todate">to date</param>
-        /// <returns></returns>
-        public SingleDoubleValue actual365fyf(long? fromdate, long? todate)
-        {
-            // create path and map variables
-            var path = "/daycount/{fromdate}/{todate}}/actual365fyf".Replace("{format}", "json").Replace("{" + "fromdate" + "}", apiInvoker.escapeString(fromdate.ToString())).Replace("{" + "todate" + "}", apiInvoker.escapeString(todate.ToString()));
-
-            // query params
-            var queryParams = new Dictionary<String, String>();
-            var headerParams = new Dictionary<String, String>();
-            var formParams = new Dictionary<String, object>();
-
-            // verify required params are set
-            if (fromdate == null || todate == null)
-            {
-                throw new ApiException(400, "missing required params");
-            }
-            try
-            {
-                if (typeof(SingleDoubleValue) == typeof(byte[]))
-                {
-                    var response = apiInvoker.invokeBinaryAPI(basePath, path, "GET", queryParams, null, headerParams, formParams);
-                    return ((object)response) as SingleDoubleValue;
-                }
-                else
-                {
-                    var response = apiInvoker.invokeAPI(basePath, path, "GET", queryParams, null, headerParams, formParams);
-                    if (response != null)
-                    {
-                        return (SingleDoubleValue)ApiInvoker.deserialize(response, typeof(SingleDoubleValue));
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-            }
-            catch (ApiException ex)
-            {
-                if (ex.ErrorCode == 404)
-                {
-                    return null;
-                }
-                else
-                {
-                    throw ex;
-                }
-            }
-        }
-        /// <summary>
         /// day count using the Actual/360 method returns the day count given a start and end date as serial numbers
         /// </summary>
         /// <param name="fromdate">from date</param>
@@ -350,15 +297,15 @@ namespace Org.Quantintel.Spectrum.Api
             }
         }
         /// <summary>
-        /// day count using the Actual/360F method returns the day count given a start and end date as serial numbers
+        /// day count fraction using the Actual/360F method returns the day count fraction given a start and end date as serial numbers
         /// </summary>
         /// <param name="fromdate">from date</param>
         /// <param name="todate">to date</param>
         /// <returns></returns>
-        public SingleLongValue actual365f(long? fromdate, long? todate)
+        public SingleDoubleValue actual365fyf(long? fromdate, long? todate)
         {
             // create path and map variables
-            var path = "/daycount/{fromdate}/{todate}}/actual365f".Replace("{format}", "json").Replace("{" + "fromdate" + "}", apiInvoker.escapeString(fromdate.ToString())).Replace("{" + "todate" + "}", apiInvoker.escapeString(todate.ToString()));
+            var path = "/daycount/{fromdate}/{todate}/actual365fyf".Replace("{format}", "json").Replace("{" + "fromdate" + "}", apiInvoker.escapeString(fromdate.ToString())).Replace("{" + "todate" + "}", apiInvoker.escapeString(todate.ToString()));
 
             // query params
             var queryParams = new Dictionary<String, String>();
@@ -372,17 +319,17 @@ namespace Org.Quantintel.Spectrum.Api
             }
             try
             {
-                if (typeof(SingleLongValue) == typeof(byte[]))
+                if (typeof(SingleDoubleValue) == typeof(byte[]))
                 {
                     var response = apiInvoker.invokeBinaryAPI(basePath, path, "GET", queryParams, null, headerParams, formParams);
-                    return ((object)response) as SingleLongValue;
+                    return ((object)response) as SingleDoubleValue;
                 }
                 else
                 {
                     var response = apiInvoker.invokeAPI(basePath, path, "GET", queryParams, null, headerParams, formParams);
                     if (response != null)
                     {
-                        return (SingleLongValue)ApiInvoker.deserialize(response, typeof(SingleLongValue));
+                        return (SingleDoubleValue)ApiInvoker.deserialize(response, typeof(SingleDoubleValue));
                     }
                     else
                     {
@@ -436,6 +383,59 @@ namespace Org.Quantintel.Spectrum.Api
                     if (response != null)
                     {
                         return (SingleDoubleValue)ApiInvoker.deserialize(response, typeof(SingleDoubleValue));
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (ApiException ex)
+            {
+                if (ex.ErrorCode == 404)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+        }
+        /// <summary>
+        /// day count using the Actual/360F method returns the day count given a start and end date as serial numbers
+        /// </summary>
+        /// <param name="fromdate">from date</param>
+        /// <param name="todate">to date</param>
+        /// <returns></returns>
+        public SingleLongValue actual365f(long? fromdate, long? todate)
+        {
+            // create path and map variables
+            var path = "/daycount/{fromdate}/{todate}/actual365f".Replace("{format}", "json").Replace("{" + "fromdate" + "}", apiInvoker.escapeString(fromdate.ToString())).Replace("{" + "todate" + "}", apiInvoker.escapeString(todate.ToString()));
+
+            // query params
+            var queryParams = new Dictionary<String, String>();
+            var headerParams = new Dictionary<String, String>();
+            var formParams = new Dictionary<String, object>();
+
+            // verify required params are set
+            if (fromdate == null || todate == null)
+            {
+                throw new ApiException(400, "missing required params");
+            }
+            try
+            {
+                if (typeof(SingleLongValue) == typeof(byte[]))
+                {
+                    var response = apiInvoker.invokeBinaryAPI(basePath, path, "GET", queryParams, null, headerParams, formParams);
+                    return ((object)response) as SingleLongValue;
+                }
+                else
+                {
+                    var response = apiInvoker.invokeAPI(basePath, path, "GET", queryParams, null, headerParams, formParams);
+                    if (response != null)
+                    {
+                        return (SingleLongValue)ApiInvoker.deserialize(response, typeof(SingleLongValue));
                     }
                     else
                     {
