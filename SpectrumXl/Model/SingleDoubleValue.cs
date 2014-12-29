623bf91daa3c2e597ea24a2d45d1028239460d05
Paul Bernard <paul.bernard@teleapp.com>
2014-12-29 14:02:25 -0500

    init

---
+++ SpectrumXl/Model/SingleDoubleValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Org.Quantintel.Spectrum.Model
+{
+    public class SingleDoubleValue
+    {
+        public double? value { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class SingleDoubleValue {\n");
+            sb.Append("  value: ").Append(value).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+    }
+}
